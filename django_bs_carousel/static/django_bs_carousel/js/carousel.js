3f2431c0d625264d707772f6338039818aabea72
james miller <jamesstewartmiller@gmail.com>
2026-08-12 20:06:54 +0100

    removed build and updated worker filename in carousel.js

--- django_bs_carousel/static/django_bs_carousel/js/carousel.js
+++ django_bs_carousel/static/django_bs_carousel/js/carousel.js
@@ -257,7 +257,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const screenSize = window.innerWidth < 500 ? imageSizeSmall : imageSizeLarge;
         const siteUrl = location.protocol + "//" + location.host + "/imgurl/";
         const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-        const ImageLoaderWorker = new Worker('/static/django_bs_carousel/js/il_min.js', {'type': 'classic', 'credentials': 'same-origin'});
+        const ImageLoaderWorker = new Worker('/static/django_bs_carousel/js/imageLoader.js', {'type': 'classic', 'credentials': 'same-origin'});
         var iteration = 0;
         const webpSupport = Modernizr.webp;
         let closing = false;
